40e5391f1433646f4a772b323755fdd2f860c975
Andrew Polhill <2196085+thatguynamedandy@users.noreply.github.com>
2020-12-31 10:18:15 +0000

    fix(package): adds shoutier deprecation notice

--- projects/canopy/scripts/postinstall.js
+++ projects/canopy/scripts/postinstall.js
@@ -1,7 +1,19 @@
 console.warn(`
-⚠️ This package is now deprecated please migrate to:
+***************************************************************
+************************* ⚠️ WARNING ⚠️ *************************
+***************************************************************
+
+THIS PACKAGE IS DEPRECATED AND WILL BE REMOVED ON 31ST JAN 2021
+
+If you have ignored this message previously now is the time to
+take action. As of Jan 31st this package will no longer exist.
+
+please migrate to:
 https://github.com/Legal-and-General/canopy
 
 For more information please refer to:
 https://github.com/canopy-collective/canopy#deprecated
+
+***************************************************************
+***************************************************************
 `)
